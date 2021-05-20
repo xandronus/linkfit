ace4bb42a955e177f800e21e986f771e002a992f
Xandronus <xandronus@gmail.com>
2021-05-19 20:12:00 -0600

    fix async

--- linkfitapp/pages/api/synchealth.js
+++ linkfitapp/pages/api/synchealth.js
@@ -1,7 +1,8 @@
 import * as database from "../../util/mongdb.js"
 
 export default (req, res) => {
-  database.connect(async() => {
+  var success = false
+  database.connect(() => {
     const apikey = req.headers.api_key
     if (!apikey || apikey !== process.env.API_KEY) {
       res.status(401).json({success: false, message: 'Authorization failure.'});
@@ -9,12 +10,15 @@ export default (req, res) => {
     else {
       if (req.method === 'POST') {
           console.log(`POST /synchealth => ${JSON.stringify(req.body)}`);            
-          await database.addOrGetAccount(req.body.fitbitid, req.body.cryptoaddr);
-          await database.syncHealthData(req.body);
-          res.status(200).json({success: true});        
-      } else {
-          res.status(400).json({success: false, message: 'Only supports POST method'});            
+          database.addOrGetAccount(req.body.fitbitid, req.body.cryptoaddr);
+          database.syncHealthData(req.body);
+          success = true;
       }        
     }
-  })
+  }).then( () =>{
+    if (success)
+      res.status(200).json({success:true});
+    else
+      res.status(400).json({success:false});
+  });
 }
