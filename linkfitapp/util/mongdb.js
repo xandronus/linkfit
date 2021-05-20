e667230fe8c98e00bfe6d242c723d463bf6bbec1
Xandronus <xandronus@gmail.com>
2021-05-19 20:48:49 -0600

    update

--- linkfitapp/util/mongdb.js
+++ linkfitapp/util/mongdb.js
@@ -23,14 +23,13 @@ async function createAccount(fbid, cpid, date) {
     const model = DbSchema.getModels();
     var id = newId();
     var newAccount = new model.Account({_id: id, fitbitid:fbid, cryptoaddr:cpid, timestamp:date});    
-    newAccount.save(function(err) {
+    await newAccount.save(function(err) {
         if (err) {
           console.log(`Failed to save account ${id}`);
         } else {
-          return newAccount;
+          console.log(`Successfully created new account ${id}`);
         }
     });
-    return newAccount;
 }
 
 export async function addOrGetAccount(fbid, cpid) {
@@ -38,15 +37,14 @@ export async function addOrGetAccount(fbid, cpid) {
     const model = DbSchema.getModels();
     if (!model || !model.Account) {
         console.log('calling create account');
-        return createAccount(fbid, cpid, Date.now());    
+        await createAccount(fbid, cpid, Date.now());    
     } else {
         var search = {fitbitid: fbid, cryptoaddr: cpid};
-        model.Account.findOne(search, function(err, account) {
+        model.Account.findOne(search, async function(err, account) {
             if (err || !account) {
-                return createAccount(fbid, cpid, Date.now());
+                await createAccount(fbid, cpid, Date.now());
             } else {
                 console.log('found account');
-                return account;
             }
         });
     }
@@ -57,7 +55,7 @@ async function createHealthData(health, date) {
     console.log(`Creating new daily health record ${id}`);
     const model = DbSchema.getModels();
     var newhealthData = new model.HealthData({_id: id, fitbitid:health.fitbitid, cryptoaddr:health.cryptoaddr, timestamp:date, steps:health.steps, claimed:false});
-    newhealthData.save(function(err){
+    await newhealthData.save(function(err){
         if (err) {
             console.log(`Error creating health data ${newhealthData._id} - ${err.message}`);
         }
@@ -73,7 +71,7 @@ export async function syncHealthData(health) {
     var date = new Date(health.timestamp);
 
     if (!model.HealthData) {
-        createHealthData(health, date);
+        await createHealthData(health, date);
     } else {
         // get current record for today
         var search = {
@@ -85,15 +83,15 @@ export async function syncHealthData(health) {
             }
         };
         console.log(`Health search filter: ${JSON.stringify(search)}`);
-        model.HealthData.findOne(search, function(err, healthData) {
+        model.HealthData.findOne(search, async function(err, healthData) {
             if (err || !healthData) {
-                createHealthData(health, date);
+                await createHealthData(health, date);
             } else {
                 console.log(`Updating daily health record ${healthData._id}`);
                 if (healthData.steps !== health.steps) {
                     healthData.steps = health.steps;
                     healthData.timestamp = date;
-                    healthData.save(function(err){
+                    await healthData.save(function(err){
                         if (err) {
                             console.log(`Error updating health data ${healthData._id}`);
                         }
