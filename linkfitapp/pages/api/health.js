7e4a1f6e77158f61c03d2defd79f5552fdad7e2a
Xandronus <xandronus@gmail.com>
2021-05-19 20:31:02 -0600

    refactor other endpoints

--- linkfitapp/pages/api/health.js
+++ linkfitapp/pages/api/health.js
@@ -1,13 +1,12 @@
 import * as database from "../../util/mongdb.js"
 import {DbSchema} from '../../util/mongodbschema'
 
-export default (req, res) => {
-  database.connect(async() => {
-      const model = DbSchema.getModels()
-      if (model) {
-            res.status(200).json({success: true, message: 'Healthy'});
-      } else {
-        res.status(400).json({success: false, message: 'No mongo model'});
-      }
-  });
+export default async (req, res) => {
+  await database.connect();
+  const model = DbSchema.getModels()
+  if (model) {
+    res.status(200).json({success: true, message: 'Healthy'});
+  } else {
+    res.status(400).json({success: false, message: 'No mongo model'});
+  }
 }
