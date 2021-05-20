02fc25d13b087cf5eeb713d9ad6f40a7ee70e68f
Xandronus <xandronus@gmail.com>
2021-05-19 19:53:13 -0600

    update

--- linkfitapp/pages/api/synchealth.js
+++ linkfitapp/pages/api/synchealth.js
@@ -9,8 +9,8 @@ export default (req, res) => {
     else {
       if (req.method === 'POST') {
           console.log(`POST /synchealth => ${JSON.stringify(req.body)}`);            
-          database.addOrGetAccount(req.body.fitbitid, req.body.cryptoaddr);
-          database.syncHealthData(req.body);
+          await database.addOrGetAccount(req.body.fitbitid, req.body.cryptoaddr);
+          await database.syncHealthData(req.body);
           res.status(200).json({success: true});        
       } else {
           res.status(400).json({success: false, message: 'Only supports POST method'});            
