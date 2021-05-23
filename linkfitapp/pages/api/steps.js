4c34bef9dfc4e03a9c313f2d5f00091f128c512a
Xandronus <xandronus@gmail.com>
2021-05-23 16:30:23 -0600

    Some updates

--- linkfitapp/pages/api/steps.js
+++ linkfitapp/pages/api/steps.js
@@ -1,5 +1,6 @@
 import {parse as parseQuery} from "querystring";
 import * as database from "../../util/mongdb.js"
+import * as crypto from "../../util/crypto.js"
 
 export default async (req, res) => {
 
@@ -11,6 +12,7 @@ export default async (req, res) => {
     const url = new URL(req.url, 'http://localhost');
     const query = parseQuery(url.search.substr(1));
     console.log(`GET /steps => cryptoaddr: ${query.cryptoaddr}`);
+    var addr = await crypto.normalizeAddress(query.cryptoaddr);
     //TODO: redeem steps from mongo
     //var coinBalance = await crypto.getTokenBalance(query.cryptoaddr);
     respBody = {success:true, steps:"100"};
