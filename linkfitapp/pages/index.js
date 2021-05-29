f4ea5fe5532590d0b6c792f798d3842db204a12c
Xandronus <xandronus@gmail.com>
2021-05-29 08:58:10 -0600

    fix build

--- linkfitapp/pages/index.js
+++ linkfitapp/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import * as redeem from './components/RedeemHistory'
+import * as redeem from '../components/RedeemHistory'
 
 export default function Home() {
   return (
